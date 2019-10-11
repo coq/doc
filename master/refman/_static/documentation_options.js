e1999fac58ce5bfc1d9c11c74c26d78d94305c61
coqbot <coqbot@users.noreply.github.com>
2019-10-11 11:54:56 +0000

    Documentation of branch “master” at 53e8533b

--- master/refman/_static/documentation_options.js
+++ master/refman/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.10+alpha',
+    VERSION: '8.11+alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
