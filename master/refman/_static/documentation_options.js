42fc415203b879b74cc0bc6cfdd7e97bbe687eec
coqbot <coqbot@users.noreply.github.com>
2021-06-09 08:23:36 +0000

    Documentation of branch “master” at 0104a72d

--- master/refman/_static/documentation_options.js
+++ master/refman/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.14+alpha',
+    VERSION: '8.15+alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
